f93e57d994d930bc4cd4dc26b59b30329c43f9db
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-22 02:22:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
-
-
+def my_collect
+  empty_array []
+  counter = 0 
+end
+  my_collect(empty_array).map do |x|
+    counter += 1 
+  end
+  counter = 0 
+end
+def my_collect(languages)
+  languages = (my_collect).map do |language|
+  end
+  yield x
+end
+return language
+end
